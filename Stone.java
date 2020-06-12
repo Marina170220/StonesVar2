8e95b1ea4d7f20341bbadceec8007332914d0bda
Marina <petrukmn@yandex.ru>
2020-06-12 17:28:26 +0300

    Stone class

---
+++ Stone.java
@@ -0,0 +1,55 @@
+public class Stone implements Comparable<Stone> {
+
+        String name;
+        byte karat;
+        float karatCost;
+        Float stonePrice;
+        String transparency;
+
+        public Stone(String name, byte karat, float karatCost, String transparency) {
+            this.name = name;
+            this.karat = karat;
+            this.karatCost = karatCost;
+            this.transparency = transparency;
+
+            this.stonePrice = this.karat * this.karatCost;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public byte getKarat() {
+            return karat;
+        }
+
+        public Float getStonePrice() {
+            return stonePrice;
+        }
+
+        @Override
+        public String toString() {
+            return "Stone: " + name +
+                    ", karat = " + karat +
+                    ", karatCost = " + karatCost +
+                    ", stonePrice =" + stonePrice +
+                    ", transparency: " + transparency + "\n";
+        }
+
+        @Override
+        public int compareTo(Stone s) {
+            return stonePrice.compareTo(s.getStonePrice());
+        }
+}
+class PreciousStone extends Stone{
+
+    public PreciousStone(String name, byte karat, float karatCost, String transparency) {
+        super(name, karat, karatCost, transparency);
+    }
+}
+class SemipreciousStone extends Stone{
+
+    public SemipreciousStone(String name, byte karat, float karatCost, String transparency) {
+        super(name, karat, karatCost, transparency);
+    }
+}
