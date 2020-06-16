f354c2f4dcac1f492b9a07e3b36b1efcc0d52b4c
Marina <petrukmn@yandex.ru>
2020-06-16 12:24:22 +0300

    Main method

--- Main.java
+++ Main.java
@@ -5,42 +5,37 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Stone> collectionStone;
+    static ArrayList<Stone> preciousStoneCollection;
+    static ArrayList<Stone> semipreciousStoneCollection;
 
     public static void main(String[] args) {
+        collectionStone = new ArrayList<>();
+        preciousStoneCollection = new ArrayList<>();
+        semipreciousStoneCollection = new ArrayList<>();
+        Selection selector = new Selection();
 
         try {
-            Necklace.necklace = new ArrayList<Stone>();
             Scanner scanner = new Scanner(new File("src\\PreciousStones.txt"));
             while (scanner.hasNext()) {
-                Necklace.necklace.add(new PreciousStone(scanner.next(), scanner.nextByte(), scanner.nextFloat(), scanner.next()));
+                preciousStoneCollection.add(new Stone(scanner.next(), scanner.nextByte(), scanner.nextFloat(), scanner.next()));
             }
             scanner.close();
 
             Scanner scanner1 = new Scanner(new File("src\\SemiPreciousStones.txt"));
             while (scanner1.hasNext()) {
-                Necklace.necklace.add(new SemipreciousStone(scanner1.next(), scanner1.nextByte(), scanner1.nextFloat(), scanner1.next()));
+                semipreciousStoneCollection.add(new Stone(scanner1.next(), scanner1.nextByte(), scanner1.nextFloat(), scanner1.next()));
             }
             scanner1.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        collectionStone.addAll(preciousStoneCollection);
+        collectionStone.addAll(semipreciousStoneCollection);
+        Collections.sort(collectionStone);
+        System.out.println(collectionStone);
 
-        Necklace.necklace.removeIf(s -> s.getKarat() < 2 | s.getKarat() > 8);
-        Collections.sort(Necklace.necklace);
-        System.out.println();
-        System.out.println(Necklace.necklace);
-
-        System.out.print("Transparent stones in necklace: ");
-
-        for (Stone s : Necklace.necklace) {
-            if (s.transparency.contains("parent")) {
-                System.out.print(s.getName() + ", ");
-            }
-        }
-
-        Necklace.findTotalCost();
-        Necklace.findTotalKarat();
+        selector.selectedStones(selector.chooseStoneType());
     }
 }
-
