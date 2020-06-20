81c04771672dd2cea1f5de919980c4e3bb919b4e
Marina <petrukmn@yandex.ru>
2020-06-20 17:37:21 +0300

    Add parameters in chooseStoneType method

--- DialogBox.java
+++ DialogBox.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public interface DialogBox {
 
-    Collection<Stone> chooseStoneType();
+    Collection<Stone> chooseStoneType(ArrayList<Stone> list1, ArrayList<Stone> list2, ArrayList<Stone> list3);
 
     void selectedStones(ArrayList<Stone> c);
 }
